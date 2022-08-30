c0dc8c56f5c4c79b46d7101a98fcf305bdefad48
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-08-30 10:04:46 -0300

    feat: adição do cors à rota /login

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import * as cors from 'cors'
 
 const router = Router()
 
 const userController = new UserController()
 
-router.post('/', userController.login)
+router.post('/', cors(), userController.login)
 
 export default router
