ba423311f3fd0a7c79c11ea2e3cef31f165601b3
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-09-01 17:40:10 -0300

    feat: implementação da funcção camelize na service TeamService

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,11 +1,14 @@
 import Team from '../database/models/Team'
+import camelize from '../utils/camelize'
 
 export default class TeamService {
   public getAll = async () => {
       const teams = await Team.findAll()
+      console.log(teams)
 
       if(teams.length > 0) {
-        return { statusCode: 200, message: teams }
+        const camelizedTeams = camelize(JSON.parse(JSON.stringify(teams)))
+        return { statusCode: 200, message: camelizedTeams }
       } return { statusCode: 404, message: {message: 'No teams were found' } }
   }
 
